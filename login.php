7aae2756881523f104b5f39bc564fe8533a00dda
Leonardo <est.jhoan.santana@unimilitar.edu.co>
2021-06-02 16:00:53 -0500

    modificacion login

--- login.php
+++ login.php
@@ -1,19 +1,25 @@
 <?php
 include('ConnectionDB.php');
 $emailUsuario=$_POST['emailUsuario'];
-$sql="SELECT*FROM usuario WHERE emailUsuario='$emailUsuario';
-$result= mysqli_query($con,$sql);
-if ($result){   
-    $row=mysqli_fetch_array($result);
-    $count=mysqli_rows($result);
-    if($count!=0){
-
+$sql="SELECT * FROM usuario WHERE emailUsuario='$emailUsuario'";
+$result=mysqli_query($con,$sql);
+if ($result) {
+    $row=mysql_fetch_array($result);
+    $count=mysqli_num_rows($result);
+    if ($count!=0) {
+        $contrasena=$_POST['contrasena'];
+        if ($row['contrasena']!=$contrasena) {
+            header('Location: ../login.php?e=3');
+        }else{
+            session_start();
+            $_SESSION['codeUsuario']=$row['codeUsuario'];
+            $_SESSION['emailUsuario']=$row['emailUsuario'];
+            $_SESSION['nombreUsuario']=$row['nombreUsuario'];
+            header('Location: ../login.php?e=2');
+        }
     }else{
-        header('../login.php?e=2');
-        
+        header('Location: ../login.php?e=2');
     }
 }else{
-        header('../login.php?e=1');
-        
-    }
-?>
+    header('Location: ../login.php?e=1');
+}>
