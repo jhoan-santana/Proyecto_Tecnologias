db80b6baebe8eee04e77e35d510cdcb86d7f5e3b
Sofi <laura.cuellar@unimilitar.edu.co>
2021-06-03 02:16:11 -0500

    actualizacion

--- index.php
+++ index.php
@@ -7,11 +7,30 @@
 	<title>PCREA</title>
 	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Sen&display=swap" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<html lang="en">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Proyecto</title>
+    <link rel="stylesheet" href="css/estilos.css" />
+    <link rel="shortcut icon" href="assets/logo2.png" type="image/x-icon" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap"
+      rel="stylesheet"
+/>
 </head>
 <body>
 	<header>
+	<nav>
+        <a href="login.php">Iniciar sesión</a>
+        <img src="assets/logo.png" alt="" class="imagen-logo" />
+        <a href="Register.php">Registrate en pcrea</a>
+    </nav>
 		<div class="logo-place"><a href="index.php"><img src="assets/logo.png"></a></div>
 		<div class="search-place">
 			<input type="text" id="idbusqueda" placeholder="Encuenta todo lo que necesitas...">
@@ -33,6 +52,18 @@
 				<a href="carrito.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
 			</div>
 		</div>
+		<div class="wave" style="height: 150px; overflow: hidden;">
+        <svg
+          viewBox="0 0 500 150"
+          preserveAspectRatio="none"
+          style="height: 100%; width: 100%;"
+        >
+          <path
+            d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+            style="stroke: none; fill: #ffff;"
+          ></path>
+        </svg>
+      </div>
 	</header>
 	<div class="main-content">
 		<div class="content-page">
