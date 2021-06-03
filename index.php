cacd790c4dc49c5912f48931dd5c192d20d4e201
Sofi <laura.cuellar@unimilitar.edu.co>
2021-06-03 13:18:24 -0500

    Index

--- index.php
+++ index.php
@@ -25,30 +25,26 @@
 </head>
 <body>
 	<header>
-    <nav>
-      <img src="assets/logo.png" alt="" class="imagen-logo" />
-          <div class="options-place">
-      <div class="item-option" title="Mis compras">
-        <a href="carrito.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
-      </div>
-    </div>
-    </nav>
-	<div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #ffff;"> </path> </svg></div>
+		<nav>           
+            <img src="assets/logo.png" alt="" class= "imagen-logo">
+            <a href="carrito.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+  		</nav>
+		<div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #ffff;"> </path> </svg></div>
 	</header>
 
-	<div class="main-content">
-		<div class="content-page">
-			<h1 class = "titulo">Tarjeta gráfica</h1>
-			<div class= contenedor-grafica>
-              <div class= "contenido-textos">
-                  <h3><span>1</span>¿Qué son las tarjetas gráficas?</h3>
-                  <p>La tarjeta gráfica o tarjeta de vídeo de un componente que viene integrado en la placa base del PC o se instala a parte para ampliar sus capacidades. Concretamente, esta tarjeta está dedicada al procesamiento de datos relacionados con el vídeo y las imágenes que se están reproduciendo en el ordenador. </p>
-              </div>
-         	 </div>
-			<div class="products-list" id="space-list">
+	
+	<div class="contenedor grafica">
+		<h1 class = "titulo">Tarjeta gráfica</h1>
+		<div class= contenedor-grafica>
+			<div class= "contenido-textos">
+				<h3><span>1</span>¿Qué son las tarjetas gráficas?</h3>
+				<p>La tarjeta gráfica o tarjeta de vídeo de un componente que viene integrado en la placa base del PC o se instala a parte para ampliar sus capacidades. Concretamente, esta tarjeta está dedicada al procesamiento de datos relacionados con el vídeo y las imágenes que se están reproduciendo en el ordenador. </p>
 			</div>
+			</div>
+		<div class="products-list" id="space-list">
 		</div>
 	</div>
+
 
 
 	<script type="text/javascript">
@@ -65,7 +61,7 @@
 						'<div class="product-box">'+
 							'<a href="producto.php?p='+data.datos[i].codpro+'">'+
 								'<div class="product">'+
-									'<img src="assets/products/'+data.datos[i].rutimapro+'">'+
+									'<img src="assets/products/Chasis/'+data.datos[i].rutimapro+'">'+
 									'<div class="detail-title">'+data.datos[i].nompro+'</div>'+
 									'<div class="detail-description">'+data.datos[i].despro+'</div>'+
 									'<div class="detail-price">'+formato_precio(data.datos[i].prepro)+'</div>'+
@@ -80,16 +76,12 @@
 				}
 			});
 		});
-
-
 		function formato_precio(valor){
 			//10.99
 			let svalor=valor.toString();
 			let array=svalor.split(".");
 			return "S/. "+array[0]+".<span>"+array[1]+"</span>";
 		}
-
-		
 	</script>
 </body>
 </html>
