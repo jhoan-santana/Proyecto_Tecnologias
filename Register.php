652f2d0e782191a523f5c5f6afe0125292b4a7c7
linkinrasp <est.andresf.escobar@unimilitar.edu.co>
2021-06-02 21:01:55 -0500

    regsiter.php

--- Register.php
+++ Register.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PCreate</title>
-    <link rel="stylesheet" href="css/estilosregister.css">
+    <link rel="stylesheet" href="css/estilos.css">
     <link rel="shortcut icon" href="assets/logo2.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -14,21 +14,24 @@
 </head>
 
 <body>
-  <header>
+<header>
+
+  <nav>                
+      <img src="assets/logo.png" alt="" class= "imagen-logo">
+    </nav>  
+   
+
     <div class="form-group"> 
+    <h1>Registrate</h1>
       <form action="servicios/login.php" method="POST">
 
-        <label for="name" class="label">Nombre del usuario</label>
-        <input type="text" name="NameUsuario">
+      <input type="text" name="NombreUsuario" placeholder="Nombre de Usuario">
 
-        <label for="name" class="label">Apellido del usuario</label>
-        <input type="text" name="lUsuario">
+      <input type="text" name="ApellidoUsuario" placeholder="Apellido de Usuari">
 
-        <label for="email" class="label">Correo electronico</label>
-        <input type="text" name="emailUsuario">
+      <input type="text" name="EmailUsuario" placeholder="Correo electronico">
 
-        <label for="password" class="label" >Contraseña</label>
-        <input type="password" name="contrasena">
+      <input type="text" name="Contrasena" placeholder="Contraseña">
 
         <?php
           if (isset($_GET['e'])) { 
@@ -50,6 +53,7 @@
         <button type="submit">Ingresar</button>
       </form>
     </div>
+    <div class= "wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #ffff;"></path></svg></div>
   </header>
   </body>
 </html>
