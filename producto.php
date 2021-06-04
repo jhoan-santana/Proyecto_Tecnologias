64024043568b7a17dc7b5c246873d0d1781f6aac
Sofi <laura.cuellar@unimilitar.edu.co>
2021-06-04 12:02:18 -0500

    calificacion

--- producto.php
+++ producto.php
@@ -53,6 +53,19 @@
 					<h1 id="idprice">$ 123.<span>99</span></h1>
 					<h3 id="iddescription">Descripcion del producto</h3>
 					<button onclick="iniciar_compra()">Agregar a mi configuracion</button>
+					<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+					<p class="clasificacion">
+						<input id="radio1" type="radio" name="estrellas" value="1"><!--
+						--><label for="radio1">★</label><!--
+						--><input id="radio2" type="radio" name="estrellas" value="2"><!--
+						--><label for="radio2">★</label><!--
+						--><input id="radio3" type="radio" name="estrellas" value="3"><!--
+						--><label for="radio3">★</label><!--
+						--><input id="radio4" type="radio" name="estrellas" value="4"><!--
+						--><label for="radio4">★</label><!--
+						--><input id="radio5" type="radio" name="estrellas" value="5"><!--
+						--><label for="radio5">★</label>
+					</p>
 				</div>
 			</section>
 			<div class="titulo">Productos destacados</div>
