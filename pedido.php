8e428bc107ce92a616f809c2a7b91db401d86157
Leonardo <est.jhoan.santana@unimilitar.edu.co>
2021-06-04 00:37:46 -0500

    relacion aspecto

--- pedido.php
+++ pedido.php
@@ -8,9 +8,24 @@
 <html>
 <head>
 	<title>PCREA</title>
+  <html lang="en">
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Sen&display=swap" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+  <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/estilos.css" />
+  <link rel="shortcut icon" href="assets/logo2.png" type="image/x-icon" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+    crossorigin="anonymous"/>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" 
+    rel="stylesheet"/>
+
 	<link rel="stylesheet" href="librerias/fontawesome/css/all.css">
 </head>
 <body>
