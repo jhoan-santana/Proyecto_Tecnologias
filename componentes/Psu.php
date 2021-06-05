e0e497ba0484a696d9ed52b49021137a1f0b1f70
CLHISTEK <est.gabriel.chaves@unimilitar.edu.co>
2021-06-04 22:20:38 -0500

    llamame

--- componentes/Psu.php
+++ componentes/Psu.php
@@ -9,11 +9,11 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Sen&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="css/estilos.css" />
-  <link rel="shortcut icon" href="assets/logo2.png" type="image/x-icon" />
+  <link rel="stylesheet" href="../css/estilos.css">
+  <link rel="shortcut icon" href="../assets/logo2.png" type="image/x-icon" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <link
     rel="stylesheet"
@@ -23,7 +23,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" 
     rel="stylesheet"/>
 
-	<link rel="stylesheet" href="librerias/fontawesome/css/all.css">
+	<link rel="stylesheet" href="../librerias/fontawesome/css/all.css">
 	
 </head>
 <body>
@@ -41,13 +41,13 @@
 			<?php
 			}
 			?>
-			<div class="logo-place"><a href="inicio.php"><img src="assets/logo.png"></a></div>
+			<div class="logo-place"><a href="inicio.php"><img src="../assets/logo.png"></a></div>
 			<div class="item-option" title="Mi PC">
-				<a href="carrito.php"><i class="fas fa-desktop" aria-hidden="true"></i></a>
+				<a href="../Mipc/carrito.php"><i class="fas fa-desktop" aria-hidden="true"></i></a>
 				
 			</div>
 			
-			<a class = "logout" href="logout.php">Cierra Sesión </a>
+			<a class = "logout" href="../servicios/logout.php">Cierra Sesión </a>
 		</div>
 		
 		<div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #ffff;"> </path> </svg></div>
@@ -71,7 +71,7 @@
 	<script type="text/javascript">
 		$(document).ready(function(){
 			$.ajax({
-				url:'servicios/producto/get_all_products6.php',
+				url:'../servicios/producto/get_all_products6.php',
 				type:'POST',
 				data:{},
 				success:function(data){
@@ -80,7 +80,7 @@
 					for (var i = 0; i < data.datos.length; i++) {
 						html+=
 						'<div class="product-box">'+
-							'<a href="producto.php?p='+data.datos[i].codpro+'">'+
+							'<a href="../Mipc/producto.php?p='+data.datos[i].codpro+'">'+
 								'<div class="product">'+
 									'<img src="assets/products/'+data.datos[i].rutimapro+'">'+
 									'<div class="detail-title">'+data.datos[i].nompro+'</div>'+
--- componentes/inicio.php
+++ componentes/inicio.php
@@ -9,11 +9,11 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Sen&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="../css/estilos.css">
-  <link rel="shortcut icon" href="assets/logo2.png" type="image/x-icon" />
+  <link rel="shortcut icon" href="../assets/logo2.png" type="image/x-icon" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <link
     rel="stylesheet"
@@ -23,7 +23,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" 
     rel="stylesheet"/>
 
-	<link rel="stylesheet" href="librerias/fontawesome/css/all.css">
+	<link rel="stylesheet" href="../librerias/fontawesome/css/all.css">
 	
 </head>
 <body>
@@ -41,13 +41,13 @@
 			<?php
 			}
 			?>
-			<div class="logo-place"><a href="inicio.php"><img src="assets/logo.png"></a></div>
+			<div class="logo-place"><a href="inicio.php"><img src="../assets/logo.png"></a></div>
 			<div class="item-option" title="Mi PC">
-				<a href="carrito.php"><i class="fas fa-desktop" aria-hidden="true"></i></a>
+				<a href="../Mipc/carrito.php"><i class="fas fa-desktop" aria-hidden="true"></i></a>
 				
 			</div>
 			
-			<a class = "logout" href="logout.php">Cierra Sesión </a>
+			<a class = "logout" href="../servicios/logout.php">Cierra Sesión </a>
 		</div>
 
 		<div class="container">
