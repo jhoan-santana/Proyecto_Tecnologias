105ca78ee3e03fd8f42d10bba11560ed70dace07
Sofi <laura.cuellar@unimilitar.edu.co>
2021-06-04 22:28:01 -0500

    log

--- Registro/loginadmin.php
+++ Registro/loginadmin.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PCreate</title>
     <link rel="stylesheet" href="../css/estilos.css">
-    <link rel="shortcut icon" href="assets/logo2.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/logo2.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
